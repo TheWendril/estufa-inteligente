d84499b625a96c80be67a4a141aaaf78ff985531
TheWendril <wendril2001avila@alu.ufc.br>
2021-11-27 18:11:11 -0300

    Resolving data struct

--- src/EITP/Commander/EITPCommander.py
+++ src/EITP/Commander/EITPCommander.py
@@ -1,4 +1,4 @@
-from src.EITP.EITPBaseData import EITPOperation, EITPBaseData, EITPConnectedClient
+from src.EITP.EITPBaseData import EITPOperation, EITPBaseData, EITPConnectedClient, EITPType
 from src.EITP.EITPMessengerBase import EITPMessengerServer
 from abc import ABC
 import random
@@ -33,13 +33,17 @@ class CommandInvoker:
 class GetCommand(Command):
 
     def execute(self, server: EITPMessengerServer, message: EITPBaseData) -> None:
-        pass
+        for client in server.connected_clients:
+            if client.id == message.header.recipient:
+                server.socket_server.send(str(client.last_data))
 
 
 class SendCommand(Command):
 
     def execute(self, server: EITPMessengerServer, message: EITPBaseData) -> None:
-        pass
+        for client in server.connected_clients:
+            if client.id == message.header.sender:
+                client.last_data = message.body.data
 
 
 class SyncCommand(Command):
@@ -68,7 +72,11 @@ class ConnectCommand(Command):
         new_client.type = message.header.type
         server.connected_clients.insert(0, new_client)
         # !! add a ip address in future
-        server.socket_server.send(str(new_client.id))
+
+        if new_client.type == EITPType.CLIENT:
+            server.socket_server.send(str(server.connected_clients))
+        else:
+            server.socket_server.send(str(new_client.id))
 
 
 class DisconnectCommand(Command):
